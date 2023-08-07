ea17282f3b378488ac5d493a83240c92f17a4758
halyssonJr <halysson1007@gmail.com>
2023-08-07 10:16:55 -0300

    corrgindo inicialização do transmissor

--- WiFi /ESP_NOW/FranzininhoWiFi_Transmissor_ESP_NOW/FranzininhoWiFi_Transmissor_ESP_NOW.ino
+++ WiFi /ESP_NOW/FranzininhoWiFi_Transmissor_ESP_NOW/FranzininhoWiFi_Transmissor_ESP_NOW.ino
@@ -11,6 +11,7 @@
 
 //Substitua de acordo com o seu ESP 32
 uint8_t franzininho_recv[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}; 
+esp_now_peer_info_t peerInfo;
 
 void on_sent_data (const uint8_t *mac_addr, esp_now_send_status_t status)
 {
@@ -24,50 +25,34 @@ void on_sent_data (const uint8_t *mac_addr, esp_now_send_status_t status)
            );
   Serial.print("MAC Destino: "); 
   Serial.println(mac_str);
-  
-  if(status == ESP_NOW_SEND_SUCCESS)
-  {
-    Serial.println("Delivery Success");
-  }
-  
-  else
-  {
-    Serial.println("Delivery Fail");
-  }
 }
 void setup() 
 {
   Serial.begin (115200);
   Serial.println("Configurando o ESP NOW");
-  
-  WiFi.disconnect();
-  
-  //Configurando em modo Station
+  delay(1000);  
+
+  // Configurando o WiFi no modo Station
   WiFi.mode(WIFI_STA);
-  esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE);
-  Serial.println("Inicializa ESP NOW");
-  
-  // Caso não seja inicializado corretamente, então ...
-  if( esp_now_init() != ESP_OK)
-  {
-    Serial.println("Erro ao inicializar o ESP NOW ");
-    Serial.println("Reiniciando Franzininho em 3 segundos ...");
-    delay(3000);
-    ESP.restart();
-  }
-  //Configuramos um novo dispositivo 
-  esp_now_peer_info_t peerInfo;
-  
-  memcpy(peerInfo.peer_addr, franzininho_recv, 6);
-  peerInfo.encrypt = false;
-  
-  //Adicionamos o novo dispositivo        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK)
-  {
-    Serial.println("Falha ao adicionar um novo endereço");
+
+  // Iniciando ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Erro ao inicializar o ESP-NOW");
     return;
   }
-  esp_now_register_send_cb (on_sent_data);
+
+  esp_now_register_send_cb(on_sent_data);
+  
+  // Registrando o novo endereço
+  memcpy(peerInfo.peer_addr, franzininho_recv, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Adicionando novo dispositivo na lista de endereços do ESP-NOW       
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Falha ao adicionar");
+    return;
+  }
 }
 
 void loop() 
